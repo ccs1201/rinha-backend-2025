84dec8e46a426872a8a3baa721c3acf4b126fc80
Cleber Souza <cleber.constante@totvs.com.br>
2025-07-21 18:00:45 -0300

    Ajustes para enviar a versão interpetrada

--- src/main/java/br/com/ccs/rinha/repository/RedisPaymentRepository.java
+++ src/main/java/br/com/ccs/rinha/repository/RedisPaymentRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
